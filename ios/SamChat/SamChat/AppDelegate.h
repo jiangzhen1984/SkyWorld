500708826955f69ea0bdffeb8eca0bf241daf002
HJ <gknows@hjs-macbook-pro.local>
2016-03-23 20:37:35 +0800

    init ios project

---
+++ ios/SamChat/SamChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SamChat
+//
+//  Created by HJ on 3/23/16.
+//  Copyright © 2016 SkyWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/SamChat/SamChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SamChat
+//
+//  Created by HJ on 3/23/16.
+//  Copyright © 2016 SkyWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
