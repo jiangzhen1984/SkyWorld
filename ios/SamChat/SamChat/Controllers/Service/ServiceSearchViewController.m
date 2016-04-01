099f5a149cdcb5ba9db250f25300288744a010f3
HJ <gknows@hjs-macbook-pro.local>
2016-04-01 19:34:25 +0800

    add service search bar animation

--- ios/SamChat/SamChat/Controllers/Service/ServiceSearchViewController.m
+++ ios/SamChat/SamChat/Controllers/Service/ServiceSearchViewController.m
@@ -13,11 +13,38 @@
 
 @interface ServiceSearchViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *searchTextField;
+@property (weak, nonatomic) IBOutlet UIImageView *homeImage;
+@property (weak, nonatomic) IBOutlet UIView *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *hotQuestionTable;
+
+@property (nonatomic, strong) NSLayoutConstraint *searchBarToTop;
+@property (nonatomic, strong) NSLayoutConstraint *tableViewToTop;
+
+//- (void)hideHomeImage:(BOOL)hide withsearchBarToTop:(CGFloat)barTop duration:(CGFloat)duration;
 
 @end
 
 @implementation ServiceSearchViewController
 
+- (void)viewDidLoad
+{
+    self.searchBarToTop = [NSLayoutConstraint constraintWithItem:_searchBar
+                                                       attribute:NSLayoutAttributeTop
+                                                       relatedBy:NSLayoutRelationEqual
+                                                          toItem:self.view
+                                                       attribute:NSLayoutAttributeTop
+                                                      multiplier:1.0f
+                                                        constant:130];
+    self.tableViewToTop = [NSLayoutConstraint constraintWithItem:_hotQuestionTable
+                                                       attribute:NSLayoutAttributeTop
+                                                       relatedBy:NSLayoutRelationEqual
+                                                          toItem:self.view
+                                                       attribute:NSLayoutAttributeTop
+                                                      multiplier:1.0f
+                                                        constant:200];
+
+    [self.view addConstraints:@[self.searchBarToTop, self.tableViewToTop]];
+}
 
 - (NSString *)generateNewQuestionUrlString
 {
@@ -40,6 +67,48 @@
          failure:^(NSURLSessionDataTask *task, NSError *error){
          }];
     */
+}
+
+- (IBAction)begingEditQuestion:(UITextField *)sender
+{
+    DebugLog(@"edit beging");
+    [self hideHomeImage:YES withsearchBarToTop:20 tableToTop:80 duration:0.4f];
+    
+//    [UIView animateWithDuration:0.4f
+//                     animations:^{
+//                         [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[_searchBar]"
+//                                                                                           options:0
+//                                                                                           metrics:nil
+//                                                                                             views:NSDictionaryOfVariableBindings(_searchBar)]];
+//                         [self.view layoutIfNeeded];
+//                     }];
+    
+    
+//    [UIView animateWithDuration:0.4f
+//                     animations:^{
+//                         self.homeImage.hidden = YES;
+//                         [self.searchBarToTop setConstant:20];
+//                         [self.tableViewToTop setConstant:self.searchBar.frame.size.height+40];
+//                         [self.view layoutIfNeeded];
+//                     }];
+    
+}
+
+- (void)hideHomeImage:(BOOL)hide withsearchBarToTop:(CGFloat)barTop tableToTop:(CGFloat)tableTop duration:(CGFloat)duration;
+{
+    [UIView animateWithDuration:duration
+                     animations:^{
+                         self.homeImage.hidden = hide;
+                         [self.searchBarToTop setConstant:barTop];
+                         [self.tableViewToTop setConstant:tableTop];
+                         [self.view layoutIfNeeded];
+                     }];
+}
+
+- (IBAction)backgroundTap:(id)sender
+{
+    [self.searchTextField resignFirstResponder];
+    [self hideHomeImage:NO withsearchBarToTop:130 tableToTop:200 duration:0.4f];
 }
 
 @end
