03086f595d1120be6e8104834f0fdba6a2739015
gknows <gknows@qq.com>
2016-04-18 13:11:40 +0800

    use NSAssert to make sure SCLoginModel completion block is not nil

--- ios/SamChat/SamChat/Models/Login/SCLoginModel.m
+++ ios/SamChat/SamChat/Models/Login/SCLoginModel.m
@@ -12,6 +12,7 @@
 
 + (void)loginWithUsername:(NSString *)username password:(NSString *)password completion:(void (^)(BOOL success, SCSkyWorldError *error))completion
 {
+    NSAssert(completion != nil, @"completion block should not be nil");
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     [manager GET:[SCSkyWorldAPI urlLoginWithUsername:username passWord:password]
       parameters:nil
@@ -21,9 +22,7 @@
                 NSDictionary *response = responseObject;
                 NSInteger errorCode = [(NSNumber *)response[SKYWORLD_RET] integerValue];
                 if(errorCode){
-                    if(completion){
-                        completion(false, [SCSkyWorldError errorWithCode:errorCode]);
-                    }
+                    completion(false, [SCSkyWorldError errorWithCode:errorCode]);
                 }else{
                     SCUserProfileManager *userProfileManager = [SCUserProfileManager sharedInstance];
                     [userProfileManager saveCurrentLoginUserInformationWithSkyWorldResponse:response
@@ -31,48 +30,24 @@
                     [SCLoginModel loginEaseMobWithUsername:username password:password completion:completion];
                 }
             }else{
-                if(completion){
-                    completion(false, [SCSkyWorldError errorWithCode:SCSkyWorldErrorUnknowError]);
-                }
+                completion(false, [SCSkyWorldError errorWithCode:SCSkyWorldErrorUnknowError]);
             }
         } failure:^(NSURLSessionDataTask *task, NSError *error) {
-            if(completion){
-                completion(false, [SCSkyWorldError errorWithCode:SCSkyWorldErrorServerNotReachable]);
-            }
+            completion(false, [SCSkyWorldError errorWithCode:SCSkyWorldErrorServerNotReachable]);
         }];
 }
 
 + (void)loginEaseMobWithUsername:(NSString *)username password:(NSString *)password completion:(void (^)(BOOL success, SCSkyWorldError *error))completion
 {
+    NSAssert(completion != nil, @"completion block should not be nil");
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         EMError *error = [[EMClient sharedClient] loginWithUsername:username
                                                            password:password];
         dispatch_async(dispatch_get_main_queue(), ^{
             if (!error) {
-                //设置是否自动登录
                 [[EMClient sharedClient].options setIsAutoLogin:YES];
                 [[SCUserProfileManager sharedInstance] updateCurrentLoginUserInformationWithEaseMobStatus:SC_LOGINUSER_LOGIN];
-                
-                /*
-                 //获取数据库中数据
-                 [MBProgressHUD showHUDAddedTo:weakself.view animated:YES];
-                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                 [[EMClient sharedClient] dataMigrationTo3];
-                 dispatch_async(dispatch_get_main_queue(), ^{
-                 [[ChatDemoHelper shareHelper] asyncGroupFromServer];
-                 [[ChatDemoHelper shareHelper] asyncConversationFromDB];
-                 [[ChatDemoHelper shareHelper] asyncPushOptions];
-                 [MBProgressHUD hideAllHUDsForView:weakself.view animated:YES];
-                 //发送自动登陆状态通知
-                 [[NSNotificationCenter defaultCenter] postNotificationName:KNOTIFICATION_LOGINCHANGE object:@YES];
-                 
-                 //保存最近一次登录用户名
-                 [weakself saveLastLoginUsername];
-                 });
-                 });*/
-                if(completion){
-                    completion(true, nil);
-                }
+                completion(true, nil);
             } else {
                 [[SCUserProfileManager sharedInstance] updateCurrentLoginUserInformationWithEaseMobStatus:SC_LOGINUSER_NO_LOGIN];
                 NSInteger errorCode = SCSkyWorldErrorUnknowError;
@@ -91,9 +66,7 @@
                         errorCode = SCSkyWorldErrorUnknowError;
                         break;
                 }
-                if(completion){
-                    completion(false, [SCSkyWorldError errorWithCode:errorCode]);
-                }
+                completion(false, [SCSkyWorldError errorWithCode:errorCode]);
             }
         });
     });
