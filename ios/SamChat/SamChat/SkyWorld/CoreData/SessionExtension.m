81f7419b221f8034e9667f7fb1eb19b01313cc89
gknows <gknows@qq.com>
2016-05-16 13:31:11 +0800

    default to Chat if a session has no extension tag

--- ios/SamChat/SamChat/SkyWorld/CoreData/SessionExtension.m
+++ ios/SamChat/SamChat/SkyWorld/CoreData/SessionExtension.m
@@ -68,14 +68,29 @@
 + (BOOL)chatTagOfSession:(NSString *)sessionId inManagedObjectContext:(NSManagedObjectContext *)context
 {
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:ENTITY_SESSION_EXTENSTION];
-    request.predicate = [NSPredicate predicateWithFormat:@"(%K == %@) AND (%K == %@)",SESSION_EXTENSION_SESSION_ID, sessionId, SESSION_EXTENSION_CHAT_TAG, [NSNumber numberWithBool:YES]];
+//    request.predicate = [NSPredicate predicateWithFormat:@"(%K == %@) AND (%K == %@)",SESSION_EXTENSION_SESSION_ID, sessionId, SESSION_EXTENSION_CHAT_TAG, [NSNumber numberWithBool:YES]];
+//    NSError *error;
+//    NSArray *matches = [context executeFetchRequest:request error:&error];
+//    if ((error==nil) && ([matches count] > 0)) {
+//        return YES;
+//    }else{
+//        return NO;
+//    }
+    BOOL result = NO;
+    request.predicate = [NSPredicate predicateWithFormat:@"%K == %@",SESSION_EXTENSION_SESSION_ID, sessionId];
     NSError *error;
     NSArray *matches = [context executeFetchRequest:request error:&error];
-    if ((error==nil) && ([matches count] > 0)) {
-        return YES;
-    }else{
-        return NO;
+    if (error == nil) {
+        if ([matches count] > 0) {
+            SessionExtension *extension = [matches firstObject];
+            if ([extension.chat_tag isEqualToNumber:@(YES)]) {
+                result = YES;
+            }
+        }else{
+            result = YES; // 如果一个会话没有任何标记，则当做普通会话处理
+        }
     }
+    return result;
 }
 
 + (BOOL)serviceTagOfSession:(NSString *)sessionId inManagedObjectContext:(NSManagedObjectContext *)context
